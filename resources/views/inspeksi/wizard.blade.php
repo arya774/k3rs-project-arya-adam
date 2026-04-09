e6adc28593ba7f79d1995c67bbd642f7070950c6
arya774 <qowiyyilarya621@gmail.com>
2026-04-09 09:23:27 +0700

    Update page title to 'Remi X Astuti' wizard

--- resources/views/inspeksi/wizard.blade.php
+++ resources/views/inspeksi/wizard.blade.php
@@ -2,7 +2,7 @@
 <html>
 <head>
     <script src="https://cdn.jsdelivr.net/npm/signature_pad@4.0.0/dist/signature_pad.umd.min.js"></script>
-    <title>Remi</title>
+    <title>Remi X Astuti</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet">
     <meta name="csrf-token" content="{{ csrf_token() }}">
 
